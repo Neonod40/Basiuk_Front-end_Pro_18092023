bc52916baadcece4f46d2d0f27cfc35f5159bcee
Neonod40 <basiuk40@gmail.com>
2023-09-18 16:59:34 +0300

    comm HW_3

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let UName = prompt('Як тебе звати?', "анонім")
+const UName = prompt('Як тебе звати?', "анонім")
 alert("Hello, " +UName+"! How are you?")
+
+let a = prompt('введи перше число', 0);
+let b = prompt('введи друге число', 0);
+
+c1=Number(a)+Number(b);
+c1=(a+"+"+b+"="+c1);
+c2=Number(a)-Number(b);
+c2=(a+"-"+b+"="+c2);
+c3=Number(a)*Number(b);
+c3=(a+"*"+b+"="+c3);
+c4=Number(a)/Number(b);
+c4=(a+"/"+b+"="+c4);
+
+alert(c1 + '\n' + c2 + '\n' + c3 + '\n' + c4);
